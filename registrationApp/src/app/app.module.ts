9487030bc744af479f164d7eba8e883c4f6aa83c
OngenaePieter <pieterbracke@hotmail.com>
2020-02-20 14:51:08 +0100

    Screen Orientation

--- registrationApp/src/app/app.module.ts
+++ registrationApp/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -16,7 +16,8 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ScreenOrientation,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
   bootstrap: [AppComponent]
 })
